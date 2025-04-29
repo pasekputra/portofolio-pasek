e6b16079ab8433831f399e05bdf8d3b4018d7e21
IKOMANG ADI PUTRA PASEK <pasekadiputra05@gmail.com>
2025-04-29 23:40:44 +0800

    portfolio tailwind-css

---
+++ src/js/script.js
@@ -0,0 +1,112 @@
+// huburger
+
+const hamburger = document.querySelector('#hamburger');
+const navMenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function () {
+
+    // jika di klick akan menambhkan class hamburger-active 
+    // jika di klick kedua kalinya akan menghilangkan class hamburger-active
+    hamburger.classList.toggle('hamburger-active');
+
+
+    // 
+    navMenu.classList.toggle('hidden');
+
+
+
+
+});
+
+
+// klick di luar humburgernya
+
+window.addEventListener('click', function (e) {
+
+    // jika yang kita targer bukan nav menu daan humburger
+    if (e.target != hamburger && e.target != navMenu) {
+        hamburger.classList.remove('hamburger-active');
+
+        // 
+        navMenu.classList.add('hidden');
+    }
+}
+)
+
+// navbar fixed
+// jika window di scroll
+window.onscroll = function () {
+
+    const header = document.querySelector('header');
+    // mengambil posisi header dari ujung atas halamannya 
+    const fixedNav = header.offsetTop;
+
+    // to-top
+    const toTop = document.querySelector('#to-top');
+
+    //   jika window di scroll lebih dari posisi header
+    if (window.pageYOffset > fixedNav) {
+
+        // menambhkan class baru di header
+        header.classList.add('navbar-fixed');
+
+        // menampilkan to-top
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
+    }
+
+    // jika di posisi awal
+    else {
+        // menghilangkan class navbar-fixed
+        header.classList.remove('navbar-fixed');
+
+        // menghilangkan to-top
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
+    }
+}
+
+
+// berhubungan di classdark di config
+// dark mode toggle
+const darkToggle = document.querySelector('#dark-toggle');
+const html = document.querySelector('html');
+
+
+
+// pengecekkan
+darkToggle.addEventListener('click', function () {
+
+    // jika darkToggle di check maka tambhkan class dark di html
+    if (darkToggle.checked) {
+        html.classList.add('dark');
+
+
+        // locl storage dengan keynya theme
+        localStorage.theme = 'dark';
+    }
+
+    // jika tidak di check maka hilangkan class dark di html
+    else {
+        html.classList.remove('dark');
+
+        // local storage dengan keynya theme
+        localStorage.theme = 'light';
+    }
+});
+
+
+// pindahka posisi togle sesui dengan mode
+
+if (
+    localStorage.theme === "dark" ||
+    (!("theme" in localStorage) &&
+        window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+    darkToggle.checked = true;
+
+} else {
+    darkToggle.checked = false;
+
+
+}
